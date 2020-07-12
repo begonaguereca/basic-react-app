273fbac3991d946e16a6f489b26f262cbfb2a976
Begona Guereca <begonaguereca@gmail.com>
2020-07-12 13:22:49 -0700

    initial commit

---
+++ build/precache-manifest.e636d4dee295be5a7c7f88d63b82af4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d86c4685b9549fa459f64f8244f936b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bba1cda7c6d280e481d8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5258d7558616864792cd",
+    "url": "/static/js/2.6c0d501d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c0d501d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bba1cda7c6d280e481d8",
+    "url": "/static/js/main.8950f9da.chunk.js"
+  },
+  {
+    "revision": "65174a78c22e502f7025",
+    "url": "/static/js/runtime-main.4af14cff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
